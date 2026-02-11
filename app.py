abfa51b250f48791ede619cc399dba0bf0a7c067
Ali King 👑 <sweet65222@gmail.com>
2026-02-11 09:03:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -898,5 +898,6 @@ cleanup_thread.start()
 init_db()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    # Hugging Face default port is 7860
+    port = int(os.environ.get('PORT', 7860))
     app.run(host='0.0.0.0', port=port, debug=False)
